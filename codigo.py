fe7eaa827cc24f6e2d1eb317d7d20b9ff420f324
Kenyel <prokenyel@gmail.com>
2026-03-03 09:33:38 -0300

    v-0

--- codigo.py
+++ codigo.py
@@ -1,6 +1,18 @@
+# Passo 1: Entrar no sistema da empresa
+# Passo 2: Fazer Login
+# 3: abrir a base de dados
+# 4: cadastrar 1 produto
+# 5: Repetir o passo 4 até acabar a lista de produtos
+
 import pyautogui
 
+#.click - clica
+#.write - texto
+#.press - aperta uma tecla
+#.hotkey - aperta atalho
+
 pyautogui.PAUSE = 0.5
+
 
 
 
@@ -10,3 +22,14 @@ pyautogui.press('enter')
 link = 'https://github.com/kenyelg5'
 pyautogui.typewrite(link)
 pyautogui.press('enter')
+
+
+
+
+
+# Passo 1: Entrar no sistema da empresa
+# Passo 2: Fazer Login
+# 3: abrir a base de dados
+# 4: cadastrar 1 produto
+# 5: Repetir o passo 4 até acabar a lista de produtos
+
